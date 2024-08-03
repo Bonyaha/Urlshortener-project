7bdf48b4d92e7e79b35f3ad50ac29369b9b2f0b1
Roman <romchikus1987@gmail.com>
2024-08-03 15:16:29 +0300

    updated

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const bodyParser = require('body-parser');
 const port = process.env.PORT || 3000;
 
 app.use(cors());
-
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/public', express.static(`${process.cwd()}/public`));
 
 app.get('/', function(req, res) {
@@ -19,6 +19,50 @@ app.get('/', function(req, res) {
 // Your first API endpoint
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
+});
+
+// In-memory store for URLs
+let urlDatabase = {};
+let urlCounter = 1;
+
+app.post('/api/shorturl', function(req, res) {
+  const { url } = req.body;
+  console.log('req.body is: ', req.body);
+  console.log(`url is: ${url}`);
+
+  const urlPattern = /^(http|https):\/\/([^\/]+)/;
+  const match = url.match(urlPattern);
+
+  if (!match) {
+    return res.json({ error: 'invalid url' });
+  }
+
+  const hostname = match[2];
+
+
+  dns.lookup(hostname, (err,address) => {
+    if (err) {
+      return res.json({ error: 'invalid url' });
+    }
+    // Store URL and generate short URL
+    const shortUrl = urlCounter++;
+    urlDatabase[shortUrl] = url;
+    console.log(`Address: ${address}`);
+    res.json({ original_url: url, short_url: shortUrl });
+  });
+
+})
+
+// Endpoint to redirect to original URL
+app.get('/api/shorturl/:shortUrl', function(req, res) {
+  const shortUrl = req.params.shortUrl;
+  const originalUrl = urlDatabase[shortUrl];
+
+  if (originalUrl) {
+    res.redirect(originalUrl);
+  } else {
+    res.json({ error: 'No short URL found for the given input' });
+  }
 });
 
 app.listen(port, function() {
